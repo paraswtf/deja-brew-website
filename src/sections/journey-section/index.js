cd22a2b91c176a9570d108063a0dba794494513c
Roshan Mathew <roshanmathew0512@gmail.com>
2024-08-20 18:36:20 +0530

    final project

--- src/sections/journey-section/index.js
+++ src/sections/journey-section/index.js
@@ -9,6 +9,7 @@ function JourneySection() {
       description:
         "Rolling hills adorned with emerald green coffee bushes, basking under the golden rays of the sun. This is where our coffee journey begins - in the heart of the lush coffee fields.",
       reverse: false,
+      imagemob: "/image1mob.png",
     },
     {
       image: "/image2.png",
@@ -16,6 +17,7 @@ function JourneySection() {
       description:
         "Handpicked by skilled farmers, only the ripest cherries make the cut. This careful selection ensures that every cup embodies the essence of the bean's potential.",
       reverse: true,
+      imagemob: "/image2mob.png",
     },
     {
       image: "/image3.png",
@@ -23,6 +25,7 @@ function JourneySection() {
       description:
         "Once harvested, the cherries embark on their transformation. Some may undergo the 'washed' process, cleansing the beans of their outer layers. Others embrace the 'natural' method, allowing the sun and air to work their magic. Each process contributes distinct nuances to the final brew.",
       reverse: false,
+      imagemob: "/image3mob.png",
     },
     {
       image: "/image4.png",
@@ -30,6 +33,7 @@ function JourneySection() {
       description:
         "At our roastery, all the beans undergo a final refinement. Any lingering parchment or pulp is meticulously removed before being sent for roasting. Sorting by size and quality ensures only the best reach your cup.",
       reverse: true,
+      imagemob: "/image4mob.png",
     },
     {
       image: "/image5.png",
@@ -37,6 +41,7 @@ function JourneySection() {
       description:
         "The heart of our craft is in the roasting process. Here, green beans become aromatic, flavorful coffee you savor. Skillful roasters carefully watch over every batch, coaxing out the unique personality of each bean.",
       reverse: false,
+      imagemob: "/image5mob.png",
     },
     {
       image: "/image6.png",
@@ -44,6 +49,7 @@ function JourneySection() {
       description:
         "Precision is key. The grind size is tailored to the brewing method, maximizing optimal flavor extraction. Whether you’re brewing an espresso or a French press, our grinds are finely tuned for perfection.",
       reverse: true,
+      imagemob: "/image6mob.png",
     },
     {
       image: "/image7.png",
@@ -51,6 +57,7 @@ function JourneySection() {
       description:
         "In our world, brewing is an art form. We offer a symphony of methods, from classic drip to the bold elegance of espresso. Each technique creates a distinct melody of flavors, ensuring a truly personalized experience.",
       reverse: false,
+      imagemob: "/image7.png",
     },
     {
       image: "/image8.png",
@@ -58,6 +65,7 @@ function JourneySection() {
       description:
         "Every cup is a canvas, and our baristas are the artists. From bean to cup to you, each brew is crafted with dedication, where how it’s served is as important as how it tastes.",
       reverse: true,
+      imagemob: "/image8mob.png",
     },
   ];
 
@@ -74,6 +82,7 @@ function JourneySection() {
           title={section.title}
           description={section.description}
           reverse={section.reverse}
+          imagemob={section.imagemob}
         />
       ))}
     </div>
